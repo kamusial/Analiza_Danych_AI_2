5332d27c56a9c6d6bfdce8bf6b314eb44c1b7194
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-24 10:44:29 +0200

    '1'

--- Zjazd2_24_05_25/2_pandas_diabetes.py
+++ Zjazd2_24_05_25/2_pandas_diabetes.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('C:\\Users\\vdi-student\\Desktop\\Analiza_Danych_AI_2\\Zjazd1_12_04_25\\diabetes.csv')
 print(df)
+print(df.head(3).to_string())
+print(f'Kształt DANYCH: {df.shape}')
+print(df.describe().T.round(2).to_string())
