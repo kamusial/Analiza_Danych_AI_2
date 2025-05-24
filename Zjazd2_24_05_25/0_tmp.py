4b0f1fe6df4db572928b5cd1d6a03000a3708a93
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-05-24 12:04:22 +0200

    '1'

--- Zjazd2_24_05_25/0_tmp.py
+++ Zjazd2_24_05_25/0_tmp.py
@@ -1,2 +1,12 @@
 file_names = input('podaj nazwy:  ').split()
 print(file_names)
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('C:\\Users\\vdi-student\\Desktop\\Analiza_Danych_AI_2\\Zjazd1_12_04_25\\diabetes.csv')
+print(df)
+
+#df.loc
+
+print(df.iloc[  2:9 , 3:5   ])
