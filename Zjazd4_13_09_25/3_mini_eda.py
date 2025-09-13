4a9a5eb62e9693db8f0351c48ba33b6b5f1e0860
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-09-13 14:56:36 +0200

    '1'

--- Zjazd4_13_09_25/3_mini_eda.py
+++ Zjazd4_13_09_25/3_mini_eda.py
@@ -25,5 +25,42 @@ summary = pd.DataFrame({
     "Q1": df.quantile(0.25, numeric_only=True),
     "Q3": df.quantile(0.75, numeric_only=True)
 })
+print('Podsumowanie')
+print(summary.round(2))
+
+# Histogram czasu nauki
+plt.figure(figsize=(6, 4))
+plt.hist(df["godziny"], edgecolor='black')
+plt.xlabel('godziny')
+plt.ylabel('liczebność')
+plt.tight_layout()
+plt.show()
+
+# histogram wyników
+plt.figure(figsize=(6, 4))
+plt.hist(df["wynik"], edgecolor='black')
+plt.xlabel('wyniki [%]')
+plt.ylabel('liczebność')
+plt.tight_layout()
+plt.show()
+
+# Boxplot
+plt.boxplot([df["godziny"], df["wynik"]], labels=['godziny', 'wynik'], showmeans=True)
+plt.title('Boxplot: godziny vs wyniki')
+plt.tight_layout()
+plt.show()
+
+x = df['godziny'].values
+y = df['wynik'].values
+
+a, b = np.polyfit(x, y, 1)
+plt.figure(figsize=(6, 4))
+plt.scatter(x, y, alpha=0.8)
+plt.plot(np.sort(x), a* np.sort(x) + b)
+plt.title(f'Rozrzut (trendL y={a:.2f}x+{b:.2f})')
+plt.xlabel("godziny nauki")
+plt.ylabel("wynik [%]")
+plt.tight_layout()
+plt.show()
 
 
